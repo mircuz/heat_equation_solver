5b0da24aaafa69fc1a8e4b852e3d4ea3cf9e8eb2
Mirco <mirco.meaz@gmail.com>
2019-10-14 11:00:09 +0200

    Initial Commit

---
+++ heat_transfer_eq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  heat_transfer_eq
+//
+//  Created by Mirco Meazzo on 14/10/2019.
+//  Copyright © 2019 Mirco Meazzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
